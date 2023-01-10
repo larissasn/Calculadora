fe69883d6f253d9dc75247f01193f95f8568f4a6
larissasn <106403123+larissasn@users.noreply.github.com>
2023-01-09 21:04:16 -0300

    Segundo commit - Mudando as cores..

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
 function insertValues(num) {
-    const input = document.getElementById('input-value').value
-    document.getElementById('input-value').value = input + num
+    const input = document.getElementById("input-value").value
+    document.getElementById("input-value").value = input + num
 }
 
 function calculate() {
-    const input = document.getElementById('input-value').value
-    document.getElementById('input-value').value = eval(input)
+    const input = document.getElementById("input-value").value
+    document.getElementById("input-value").value = eval(input)
 }
 
 function clearInput() {
-    document.getElementById('input-value').value = ""
+    document.getElementById("input-value").value = ""
 }
 
 function invertValue() {
     calculate()
+
+    document.getElementById("input-value").value
+    document.getElementById("input-value").value = input * -1
+}
+function getPercentageValue(value, symbol) {
+    value = value.split(symbol);
+    value = (Number(value[0]) * Number(value[1])) / 100;
+  
+    return value;
+  }
+  
+  function getResultPercentage() {
+    let value = document.getElementById("input-value").value;
+  
+    if (value.includes("-")) {
+      const percentageValue = getPercentageValue(value, "-");
+      value = value.split("-")[0];
+      return `${value}-${percentageValue}`;
+    }
+  
+    if (value.includes("+")) {
+      const percentageValue = getPercentageValue(value, "+");
+  
+      value = value.split("+")[0];
+      return `${value}+${percentageValue}`;
+    }
+  
+    if (value.includes("*")) {
+      const percentageValue = getPercentageValue(value, "*");
+      value = value.split("*")[0];
+      return `${value}*${percentageValue}`;
+    }
+  
+    if (value.includes("/")) {
+      const percentageValue = getPercentageValue(value, "/");
+      value = value.split("/")[0];
+      return `${value}/${percentageValue}`;
+    }
+  }
+  
+  function percent() {
     
-    document.getElementById('input-value').value
-    document.getElementById('input-value').value = input * -1 
-}
-/* Falta fazer o calculo de porcentagem*/ 
-function percent() {
+    document.getElementById("input-value").value = getResultPercentage();
     calculate()
-   
-}
+  }
+  
